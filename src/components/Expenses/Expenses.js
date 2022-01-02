d6fa8faeb2892e8420f74cd320f66579566c550f
ismail <ismailmo4@gmail.com>
2022-01-02 15:20:43 +0000

    added dynamic chart

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,9 +2,10 @@ import { useState } from 'react';
 import Card from "../UI/Card";
 import ExpenseFilter from './ExpenseFilter';
 import './Expenses.css';
+import ExpensesChart from './ExpensesChart';
 import ExpensesList from './ExpensesList';
 function Expenses(props) {
-    const [year, setYear] = useState('2021');
+    const [year, setYear] = useState('2022');
 
     const yearFilterHandler = (year) => {
         setYear(year);
@@ -18,6 +19,7 @@ function Expenses(props) {
         <div>
             <Card className="expenses">
                 <ExpenseFilter selected={year} onYearChange={yearFilterHandler} />
+                <ExpensesChart expenses={filteredExpenses} />
                 <ExpensesList items={filteredExpenses} />
             </Card>
         </div>
